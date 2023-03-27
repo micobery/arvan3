52616c85bedad90aceca9faaf164690b45bf614f
milad <micobery@gmail.com>
2023-03-27 05:29:00 +0430

    Initial commit

---
+++ src/main/java/com/example/arvan3/Arvan3Application.java
@@ -0,0 +1,13 @@
+package com.example.arvan3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Arvan3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Arvan3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/arvan3/Arvan3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.arvan3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Arvan3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
